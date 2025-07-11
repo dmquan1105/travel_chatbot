f48b1b39e8275d8585aee8610c1598372d736d73
DuongMinhQuan <quandm11112005@gmail.com>
2025-07-11 15:39:20 +0700

    update .gitignore

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from travel_bot import Travel
 
 def main():
     travel_agent = Travel("gemini-2.0-flash", "google-genai", temperature=0)
-    question = "Bà Nà Hill ở đâu?"
+    question = "Nha Trang ở đâu?"
     response = travel_agent.run(question=question)
     print("=" * 50)
     print(response["output"])
